c39aed2c3a9a22f0a1017c04968adac6373b9b11
Habib Siam <habib.slg100@gmail.com>
2024-11-12 17:12:52 +0600

    Add files via upload

--- control.gd
+++ control.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var label_3: Label = $Label3
 @onready var label_4: Label = $Label4
 @onready var label_5: Label = $Label5
+@onready var health: Label = $health
 
 func  _process(delta: float) -> void:
 	label.text= str("Fps: ",Engine.get_frames_per_second())
@@ -11,3 +12,4 @@ func  _process(delta: float) -> void:
 	label_3.text=str("Total Enemy3 in scene: ",Global.total_enemy3)
 	label_4.text=str("Total Enemy1 in scene: ",Global.total_enemy1)
 	label_5.text=str("Total Enemy2 in scene: ",Global.total_enemy1)
+	health.text=str("Total health: ",Global.curr_health)
